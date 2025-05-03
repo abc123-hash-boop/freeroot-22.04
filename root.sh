79f971e597478e3c1a125b9d5d87714775b4be65
abc123-hash-boop <157674226+abc123-hash-boop@users.noreply.github.com>
2025-05-03 18:06:02 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -45,7 +45,7 @@ if [ ! -e $ROOTFS_DIR/.installed ]; then
 
   while [ ! -s "$ROOTFS_DIR/usr/local/bin/proot" ]; do
     rm $ROOTFS_DIR/usr/local/bin/proot -rf
-    wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/proot "https://raw.githubusercontent.com/foxytouxxx/freeroot/main/proot-${ARCH}"
+    wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/proot "https://github.com/abc123-hash-boop/freeroot-22.04/raw/refs/heads/main/proot-${ARCH}"
 
     if [ -s "$ROOTFS_DIR/usr/local/bin/proot" ]; then
       chmod 755 $ROOTFS_DIR/usr/local/bin/proot
